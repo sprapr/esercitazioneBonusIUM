341a30d1924295581a6858d37e25b5fc5d2c978a
sprapr <73106198+sprapr@users.noreply.github.com>
2021-05-13 14:32:09 +0200

    init commit

---
+++ app/src/main/java/com/example/esercitazionebonus/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.esercitazionebonus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.attrUsername);
+        password = findViewById(R.id.attrPassword);
+        login = findViewById(R.id.loginButton);
+
+    }
+
+
+}
