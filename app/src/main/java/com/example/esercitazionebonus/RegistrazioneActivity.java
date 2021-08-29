d72d96df4a561fbd28c1968a59c39bafb43a6340
sprapr <73106198+sprapr@users.noreply.github.com>
2021-08-29 22:14:46 +0200

    migliorato il layout

--- app/src/main/java/com/example/esercitazionebonus/RegistrazioneActivity.java
+++ app/src/main/java/com/example/esercitazionebonus/RegistrazioneActivity.java
@@ -1,13 +1,11 @@
 package com.example.esercitazionebonus;
 
-import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
